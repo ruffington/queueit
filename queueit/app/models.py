d62b6ce6344efaff92411f17a22772203e3b33fe
theAmbientOrange <jw3qz@virginia.edu>
2017-02-28 23:04:07 -0500

    All database models added... for now

--- queueit/app/models.py
+++ queueit/app/models.py
@@ -4,13 +4,25 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-	id = models.AutoField(primary_key=True)
 	user = models.ForeignKey(User)
 	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 	phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=15)
 
 class Organization(models.Model):
-	id = models.AutoField(primary_key=True)
+	members = models.ManyToManyField(User)
 	name = models.CharField(max_length=100)
 	owner = models.CharField(max_length=100)
-	members = models.ManyToManyField(User)
+
+class Queue(models.Model):
+	organization = models.ForeignKey(Organization, related_name='queue_organization')
+	organizations = models.ManyToManyField(Organization, related_name='queue_organizations')
+	name = models.CharField(max_length=100)
+	address = models.CharField(max_length=100)
+	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+	phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=15)
+
+class Position(models.Model):
+	user = models.ForeignKey(User)
+	queue = models.ForeignKey(Queue)
+	position = models.IntegerField()
+	timer = models.DurationField()
